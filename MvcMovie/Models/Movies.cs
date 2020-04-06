14c72b9ee65d8970e4724a9c0bd68585fbe2713e
Shahrzad Feghhi <feghhs@uw.edu>
2020-04-05 19:57:18 -0700

    second commit

--- MvcMovie/Models/Movies.cs
+++ MvcMovie/Models/Movies.cs
@@ -7,7 +7,7 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
